84dcd61261674748b7bb7c09f47d01ed03a82b8e
eunseo <eundeok9@gmail.com>
2023-04-27 17:16:11 +0900

    실습 코드 추가

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,14 @@
 import React, {useState} from "react";
 
 const Counter = () => {
-  // useState를 사용하여 +1, -1 버튼을 눌렀을 때 값이 증가/감소 하는 것이 나타나도록 코드를 수정해주세요!
-  let count = 0;
+  const [count, setCount] = useState(0); // count 값이 바뀔 때마다 저장해주는 state
 
   const handleDecrease = () => {
-    count = count - 1;
+    setCount(count - 1); // 상태 변화 함수 setCount에 count - 1 을 전달해주어 count를 1 감소시킨다.
   };
 
   const handleIncrease = () => {
-    count = count + 1;
+    setCount(count + 1); // 상태 변화 함수 setCount에 count + 1 을 전달해주어 count를 1 증가시킨다.
   };
 
   return (
@@ -17,6 +16,7 @@ const Counter = () => {
       <p>{count}</p>
       <button onClick={handleDecrease}>-1</button>
       <button onClick={handleIncrease}>+1</button>
+      {/* 각 버튼에 onClick 이벤트 핸들러를 부여해서, 버튼이 클릭되었을 때 각 함수가 실행되도록 함 */}
     </div>
   );
 };
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,22 +1,26 @@
 import React, {useState} from "react";
 
 const Name = () => {
-  // 이곳에 useState 구문을 작성해주세요
+  const [myName, setMyName] = useState(""); // 이름이 바뀔 때마다 저장해주는 state
+  const [nickName, setNickname] = useState(""); // 닉네임이 바뀔 때마다 저장해주는 state
 
-  const handleName = () => {
-    // 이곳에 이름을 설정하도록 하는 코드를 작성해주세요
+  const handleName = (e) => {
+    setMyName(e.target.value); // 입력된 값을 상태 변화 함수 setMyName 을 통해 myName 에 저장해준다.
   };
 
-  const handleNickName = () => {
-    // 이곳에 닉네임을 설정하도록 하는 코드를 작성해주세요
+  const handleNickName = (e) => {
+    setNickname(e.target.value); // 입력된 값을 상태 변화 함수 setNickName 을 통해 nickName 에 저장해준다.
   };
 
   return (
     <div className="name">
-      <input name="myName" placeholder="이름을 입력해주세요" onChange={handleName} />
-      <input name="nickName" placeholder="닉네임을 입력해주세요" onChange={handleNickName} />
-      <p>이름: {/* 여기 중괄호 안에 닉네임이 표시되도록 하는 코드를 작성해주세요 */}</p>
-      <p>닉네임: {/* 여기 중괄호 안에 닉네임이 표시되도록 하는 코드를 작성해주세요 */}</p>
+      {/* 각 input 태그에 onChange 이벤트 핸들러를 부여해서, 값이 변경될 때마다 각 함수를 실행시키도록 함 */}
+      <input name="myName" value={myName} placeholder="이름을 입력해주세요" onChange={handleName} />
+      <input name="nickName" value={myName} placeholder="닉네임을 입력해주세요" onChange={handleNickName} />
+
+      {/* 이름/닉네임이 입력되지 않으면 "아직 이름/닉네임이 없습니다"를, 입력되었으면 입력한 값이 출력되도록 삼항연산자를 사용함 */}
+      <p>이름: {myName === "" ? "아직 이름이 없습니다." : myName}</p>
+      <p>닉네임: {nickName === "" ? "아직 닉네임이 없습니다." : nickName}</p>
     </div>
   );
 };
